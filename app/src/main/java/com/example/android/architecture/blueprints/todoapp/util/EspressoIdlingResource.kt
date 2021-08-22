e3311888b07b001c3ad4fcacc5b4b68d1bc459db
ivVane <iv.vane@gmail.com>
2021-08-22 20:08:53 +0200

    Created an Espresso idling resources to act as a synchronization mechanism for Espresso and our long running operations

---
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/util/EspressoIdlingResource.kt
@@ -0,0 +1,36 @@
+package com.example.android.architecture.blueprints.todoapp.util
+
+import androidx.test.espresso.idling.CountingIdlingResource
+
+// Idling Resource Singleton
+object EspressoIdlingResource {
+
+    private const val RESOURCE = "GLOBAL"
+
+    @JvmField
+    val countingIdlingResource = CountingIdlingResource(RESOURCE)
+
+    fun increment() {
+        countingIdlingResource.increment()
+    }
+
+    fun decrement() {
+        if (!countingIdlingResource.isIdleNow) {
+            countingIdlingResource.decrement()
+        }
+    }
+
+}
+
+// wrapEspressoIdlingResource - Function made to avoid boilerplate code or mistakes while testing
+// in case we need to use EspressoIdlingResource singleton object more then once.
+inline fun <T> wrapsEspressoIdlingResource(function: () -> T): T {
+    // Espresso does not work well with coroutines yet. See
+    // https://github.com/Kotlin/kotlinx.coroutines/issues/982
+    EspressoIdlingResource.increment() // Set app as busy.
+    return try {
+        function()
+    } finally {
+        EspressoIdlingResource.decrement() // Set app as idle.
+    }
+}
